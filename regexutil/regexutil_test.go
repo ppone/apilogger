5e2f3b146ae17de93664db605be1763232e7486e
Parag Patel <parag.patel.one@gmail.com>
2014-09-30 17:20:57 -0500

    Added test case to test for DEFAULT CURRENT_TIMESTAMP, test case is TEST_SQLITE3_CREATE_STATEMENT_D

--- regexutil/regexutil_test.go
+++ regexutil/regexutil_test.go
@@ -1,7 +1,6 @@
 package regexutil
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
 
@@ -11,6 +10,7 @@ import (
 const TEST_SQLITE3_CREATE_STATEMENT_A = "CREATE TABLE FOO ( X INTEGER, Y TEXT PRIMARY KEY)"
 const TEST_SQLITE3_CREATE_STATEMENT_B = "CREATE TABLE FOO ( X INTEGER, Y TEXT PRIMARY KEY, Z BETA NOT NULL)"
 const TEST_SQLITE3_CREATE_STATEMENT_C = "CREATE TABLE FOO ( X INTEGER, PRIMARY KEY Y, Z BETA NOT NULL)"
+const TEST_SQLITE3_CREATE_STATEMENT_D = "CREATE TABLE FOO ( X INTEGER, Y TEXT PRIMARY KEY, Z BETA NOT NULL, T DEFAULT  CURRENT_TIMESTAMP)"
 
 func TestReturnNameGroupValueMap(t *testing.T) {
 	_, err := ParseAndReturnNameGroupValueMap(SQLITE3_CREATE_TABLE_FIRST_PASS_PARSER, TEST_SQLITE3_CREATE_STATEMENT_A)
@@ -67,6 +67,8 @@ func TestSecondPassParseCreateStatement(t *testing.T) {
 
 func TestParseCreateStatement(t *testing.T) {
 
+	//CASE A
+
 	table, columnArrayMap, err := ParseCreateStatement(TEST_SQLITE3_CREATE_STATEMENT_A)
 
 	if err != nil {
@@ -89,6 +91,8 @@ func TestParseCreateStatement(t *testing.T) {
 		t.Error("test data =>", cam)
 		t.Error("returned data =>", columnArrayMap)
 	}
+
+	//CASE B
 
 	table, columnArrayMap, err = ParseCreateStatement(TEST_SQLITE3_CREATE_STATEMENT_B)
 
@@ -115,12 +119,40 @@ func TestParseCreateStatement(t *testing.T) {
 		t.Error("returned data =>", columnArrayMap)
 	}
 
+	//CASE C
+
 	table, columnArrayMap, err = ParseCreateStatement(TEST_SQLITE3_CREATE_STATEMENT_C)
 
 	if err == nil {
 		t.Error("ERROR should be thrown")
 	}
 
-	fmt.Println(err)
+	//CASE D
+	table, columnArrayMap, err = ParseCreateStatement(TEST_SQLITE3_CREATE_STATEMENT_D)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if table != "FOO" {
+		t.Error("error table name is incorrect")
+	}
+
+	cam = []map[string]string{}
+	c1 = map[string]string{"": "X INTEGER", "columntype": "INTEGER", "columnname": "X", "constraints": ""}
+	c2 = map[string]string{"": "Y TEXT PRIMARY KEY", "columntype": "TEXT", "columnname": "Y", "constraints": "PRIMARY KEY"}
+	c3 = map[string]string{"": "Z BETA NOT NULL", "columntype": "BETA", "columnname": "Z", "constraints": "NOT NULL"}
+	c4 := map[string]string{"": "T DEFAULT  CURRENT_TIMESTAMP", "columntype": "TIMESTAMP", "columnname": "T", "constraints": "DEFAULT  CURRENT_TIMESTAMP"}
+
+	cam = append(cam, c1)
+	cam = append(cam, c2)
+	cam = append(cam, c3)
+	cam = append(cam, c4)
+
+	if !reflect.DeepEqual(columnArrayMap, cam) {
+		t.Error("ParseCreateStatement created unexpected results => test case ->", TEST_SQLITE3_CREATE_STATEMENT_D)
+		t.Error("test data =>", cam)
+		t.Error("returned data =>", columnArrayMap)
+	}
 
 }
