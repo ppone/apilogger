c574ee8fcd155e2f12cb0f82469e54189af56403
Parag Patel <parag.patel.one@gmail.com>
2014-09-30 17:27:35 -0500

    added dummy go test case

--- db/godata/godata_test.go
+++ db/godata/godata_test.go
@@ -1,1 +1,17 @@
 package godata
+
+import (
+	"fmt"
+	"testing"
+)
+
+const TEST_SQLITE3_CREATE_STATEMENT_A = "CREATE TABLE FOO ( X INTEGER, Y TEXT PRIMARY KEY)"
+
+func TestNewMetaTableFromCreateStatement(t *testing.T) {
+	meta, err := NewMetaTableFromCreateStatement(TEST_SQLITE3_CREATE_STATEMENT_A)
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Println(meta.columns)
+}
