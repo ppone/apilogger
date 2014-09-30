c443a3485ed1477eb42f07a51216342e13e0fa39
Parag Patel <parag.patel.one@gmail.com>
2014-09-30 17:35:55 -0500

    Took out GoSQL logic and data and placed it in own class files

--- db/db.go
+++ db/db.go
@@ -3,69 +3,6 @@ package db
 import _ "github.com/mattn/go-sqlite3"
 import "../sqlite"
 import "errors"
-import "strings"
-
-type GoData struct {
-	goValue interface{}
-}
-
-type GoColumn struct {
-	name    string
-	sqlType int
-	goType  int
-}
-
-type GoRow struct {
-	row []GoData
-}
-
-type GoRows struct {
-	rows []GoRow
-}
-type GoColumns struct {
-	columns []GoColumn
-}
-
-type GoMetaTable struct {
-	name     string
-	dbVendor string
-	columns  *GoColumns
-}
-
-type GoTable struct {
-	metatable *GoMetaTable
-	rows      *GoRow
-}
-
-func NewGoRow() *GoRow {
-	return &GoRow{}
-}
-
-func NewGoColumns() *GoColumns {
-	return &GoColumns{}
-}
-
-func NewGoColumnsFromCreateStatement(stmt string) error {
-	if !strings.Contains(stmt, "create table") || !strings.con
-
-}
-
-func (c *GoColumns) Add(name string, sqlType, goType int) *GoColumns {
-	data := GoColumn{name, sqlType, goType}
-	c.columns = append(c.columns, data)
-	return c
-}
-
-func (r *GoRow) Add(value interface{}) *GoRow {
-	data := GoData{value}
-	r.row = append(r.row, data)
-	return r
-}
-
-func NewGoTable(name, dbVendor string, columns *GoColumns, rows *GoRow) *GoTable {
-	return &GoTable{name, dbVendor, columns, rows}
-
-}
 
 func NewConnection(dbDriver string) (Connection, error) {
 
