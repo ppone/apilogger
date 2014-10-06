e9e1c58978a4fb8a09c230c933399b167746c52c
Parag Patel <parag.patel.one@gmail.com>
2014-10-06 16:25:39 -0500

    Added Test for New Connection

--- db/db_test.go
+++ db/db_test.go
@@ -1,11 +1,14 @@
 package db
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestNewGoTable(t *testing.T) {
-
-}
-
-func TestNewGoC(t *testing.T) {
-
+func TestNewConnection(t *testing.T) {
+	c, err := NewConnection("sqlite3")
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(c)
 }
