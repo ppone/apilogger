0028716920c1934dc5ce8c506a62282f76c4278c
Parag Patel <parag.patel.one@gmail.com>
2014-10-07 12:12:39 -0500

    Added TestUpdate to Match the Update statement in sqlite.go

--- db/sqlite/sqlite_test.go
+++ db/sqlite/sqlite_test.go
@@ -1,7 +1,6 @@
 package sqlite
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
 
@@ -13,6 +12,7 @@ const SQLITE3_TEST_CREATE_SCHEMA = "CREATE TABLE FOO (NAME TEXT, ADDRESS TEXT, I
 const SQLITE3_TEST_DROP_SCHEMA = "DROP TABLE FOO"
 const SQLITE3_TEST_INSERT = "INSERT INTO FOO (NAME,ADDRESS) VALUES (?,?);"
 const SQLITE3_TEST_SELECT = "SELECT NAME,ADDRESS FROM FOO;"
+const SQLITE3_TEST_UPDATE = "UPDATE FOO SET ADDRESS = ? WHERE NAME = ?"
 const SQLITE3_TEST_CREATE_SCHEMA_FAIL = "CREATE TABLE FOO (NAME TEXT, ADDRESS TEXT, ID2 INTEGER, CREATED_TIMESTAMP DEFAULT CURRENT_TIMESTAMP);"
 
 func TestNewConnection(t *testing.T) {
@@ -112,6 +112,60 @@ func TestInsert(t *testing.T) {
 
 }
 
+func TestUpdate(t *testing.T) {
+	conn, err := NewConnection()
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer conn.Close()
+
+	err = conn.InitTable(SQLITE3_TEST_CREATE_SCHEMA, SQLITE3_TEST_TABLE_NAME)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	n, err := conn.Insert(SQLITE3_TEST_INSERT, "parag", "austin")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if n != 1 {
+		t.Error("Row was not inserted properly ; n = ", n)
+	}
+
+	n, err = conn.Insert(SQLITE3_TEST_INSERT, "bmw", "austin")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if n != 2 {
+		t.Error("Row was not inserted properly ; n = ", n)
+	}
+
+	err = conn.Update(SQLITE3_TEST_UPDATE, "boston", "parag")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = conn.Select(SQLITE3_TEST_SELECT)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = conn.db.Exec(SQLITE3_TEST_DROP_SCHEMA)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+}
+
 func TestSelect(t *testing.T) {
 
 	conn, err := NewConnection()
@@ -133,13 +187,11 @@ func TestSelect(t *testing.T) {
 		t.Error("statement is not inserted n => ", n)
 	}
 
-	//fmt.Println("n => ", n)
-
 	if err != nil {
 		t.Error(err)
 	}
 
-	goselect, err := conn.Select(SQLITE3_TEST_SELECT)
+	_, err = conn.Select(SQLITE3_TEST_SELECT)
 
 	if err != nil {
 		t.Error(err)
@@ -147,8 +199,6 @@ func TestSelect(t *testing.T) {
 
 	//_:= goselect.GetColumns()
 	//ows := goselect.GetRows()
-
-	fmt.Println("YO YO", goselect)
 
 	_, err = conn.db.Exec(SQLITE3_TEST_DROP_SCHEMA)
 
