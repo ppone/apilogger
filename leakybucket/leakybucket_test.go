d49e1f10762dc38f471d459a8cc247c6ad4eba5d
Parag Patel <parag.patel.one@gmail.com>
2014-10-09 17:03:38 -0500

    Added first test case to leaky buckegt

--- leakybucket/leakybucket_test.go
+++ leakybucket/leakybucket_test.go
@@ -1,6 +1,40 @@
 package leakybucket
 
 import "testing"
+import "../db/sqlconstants"
+import "fmt"
 
-func Test_Open_DB(t *testing.T) {
+func TestNewBucket(t *testing.T) {
+	s, err := NewBucket("A11", 10000, 3600)
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Println(s.Name())
+
+	conn, err := newConnection()
+	if err != nil {
+		t.Error(err)
+	}
+
+	goData, err := conn.Select(sqlconstants.SQLITE3_SELECT_ALL_BUCKETS, s.Name())
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Println(goData.GetRows()[0]["CREATED_TIMESTAMP"])
+
+	err = conn.Drop(sqlconstants.SQLITE3_DROP_BUCKETS)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = conn.Close()
+
+	if err != nil {
+		t.Error(err)
+	}
+
 }
