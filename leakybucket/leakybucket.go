8418f87e445cefd5c5cfdd43eecc34f6d36151b2
Parag Patel <parag.patel.one@gmail.com>
2014-10-10 17:15:02 -0500

    Added 4 methods to interface, LastUpdated(),GoSelect(), PrintRawSQL(),RefreshBucket(), Volume().  Modified leakyBucket datastructure a bit to accomadate lastUpdated (time which leakyBucket was lastUpdated).  Added logic to NewBucket to accomadate an option to first try to load the bucket from the database first before trying to create it.  Also if a new leakyBucket is created, it will go and select the newly created bucket from the database once again to populate the lastUpdated field.  Added a defered close method to RefreshBucket to avoid and sloppiness. Implemented GoSelect() which spits out the values contained in the database for the object.  PrintRawSQL another implemented method spits it out in string form.  LastUpdated simply returns the corresponding struct field value lastUpdated. Also Updated LoadBucket to retrieve and populate the lastUpdated field.

--- leakybucket/leakybucket.go
+++ leakybucket/leakybucket.go
@@ -3,6 +3,10 @@ package leakybucket
 import "errors"
 import "../db/sqlconstants"
 import "../db"
+import "../db/godata"
+
+import "strconv"
+import "time"
 
 type LeakyBucket interface {
 	Capacity() int
@@ -12,13 +16,19 @@ type LeakyBucket interface {
 	Full() (bool, error)
 	Increase() error
 	Fill(amount int) error
+	LastUpdated() int
+	GoSelect() (godata.GoRow, error)
+	PrintRawSQL() (string, error)
+	RefreshBucket() error
+	Volume() (int, error)
 }
 
 type leakyBucket struct {
-	name      string
-	capacity  int
-	volume    int
-	timeFrame int //unix epoch seconds
+	name        string
+	capacity    int
+	volume      int
+	timeFrame   int //unix epoch seconds
+	lastUpdated int //unix epoch seconds
 }
 
 func newConnection() (db.Connection, error) {
@@ -63,23 +73,51 @@ func LoadBucket(name string) (LeakyBucket, error) {
 	capacity := ((rows[0])["CAPACITY"]).(int)
 	volume := ((rows[0])["VOLUME"]).(int)
 	timeFrame := ((rows[0])["TIMEFRAME"]).(int)
+	lastUpdated := ((rows[0])["UPDATED_TIMESTAMP"]).(int)
 
-	return &leakyBucket{name, capacity, volume, timeFrame}, nil
+	return &leakyBucket{name, capacity, volume, timeFrame, lastUpdated}, nil
 
 }
 
-func NewBucket(name string, capacity, timeFrame int) (LeakyBucket, error) {
+func NewBucket(name string, capacity, timeFrame int, loadBucketIfExists bool) (LeakyBucket, error) {
 	conn, err := initDB()
 
 	defer conn.Close()
 
-	conn.Insert(sqlconstants.SQLITE3_INSERT_INTO_BUCKET, name, capacity, 0, timeFrame)
+	if loadBucketIfExists {
+		l, err := LoadBucket(name)
+
+		if err != nil {
+			if err.Error() != "Could not load bucket, as bucket does not seem to exist in the database" {
+
+				return nil, err
+
+			}
+
+		}
+
+		if err == nil {
+			return l, nil
+		}
+
+	}
+
+	_, err = conn.Insert(sqlconstants.SQLITE3_INSERT_INTO_BUCKET, name, 0, capacity, timeFrame)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return &leakyBucket{name, capacity, 0, timeFrame}, nil
+	goSel, err := conn.Select(sqlconstants.SQLITE3_SELECT_ALL_BUCKETS, name)
+	if err != nil {
+		return nil, err
+	}
+
+	rows := goSel.GetRows()
+
+	lastUpdated := ((rows[0])["UPDATED_TIMESTAMP"]).(int)
+
+	return &leakyBucket{name, capacity, 0, timeFrame, lastUpdated}, nil
 }
 
 func (L *leakyBucket) Capacity() int {
@@ -126,10 +164,11 @@ func (L *leakyBucket) Volume() (int, error) {
 //volume = (volume - capacity / timeToRefresh) * (timeNow - timeNow)
 func (L *leakyBucket) RefreshBucket() error {
 	conn, err := newConnection()
+	defer conn.Close()
 	if err != nil {
 		return err
 	}
-	defer conn.Close()
+
 	err = conn.Update(sqlconstants.SQLITE3_REFRESH_BUCKET, L.name)
 	if err != nil {
 		return err
@@ -144,9 +183,14 @@ func (L *leakyBucket) RefreshBucket() error {
 	L.capacity = ((rows[0])["CAPACITY"]).(int)
 	L.volume = ((rows[0])["VOLUME"]).(int)
 	L.timeFrame = ((rows[0])["TIMEFRAME"]).(int)
+	L.lastUpdated = ((rows[0])["UPDATED_TIMESTAMP"]).(int)
 
 	return nil
 
+}
+
+func (L *leakyBucket) LastUpdated() int {
+	return L.lastUpdated
 }
 
 func (L *leakyBucket) Increase() error {
@@ -172,8 +216,9 @@ func (L *leakyBucket) Fill(amount int) error {
 	if err != nil {
 		return nil
 	}
+	defer conn.Close()
 
-	err = conn.Update(sqlconstants.SQLITE3_FILL_BUCKET, L.name)
+	err = conn.Update(sqlconstants.SQLITE3_FILL_BUCKET, amount, L.name)
 	if err != nil {
 		return err
 	}
@@ -181,5 +226,48 @@ func (L *leakyBucket) Fill(amount int) error {
 	L.volume += amount
 
 	return nil
+
+}
+
+func (L *leakyBucket) GoSelect() (godata.GoRow, error) {
+	conn, err := newConnection()
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	goSel, err := conn.Select(sqlconstants.SQLITE3_SELECT_ALL_BUCKETS, L.name)
+
+	if err != nil {
+		return nil, err
+	}
+
+	rows := goSel.GetRows()
+
+	if len(rows) == 0 {
+		return nil, errors.New("Database Error := row not found")
+	}
+
+	return goSel.GetRows()[0], nil
+}
+
+func (L *leakyBucket) PrintRawSQL() (string, error) {
+
+	row, err := L.GoSelect()
+
+	if err != nil {
+		return "", err
+	}
+
+	name := (row["NAME"]).(string)
+	volume := strconv.Itoa((row["VOLUME"]).(int))
+	timeFrame := strconv.Itoa((row["TIMEFRAME"]).(int))
+	lastUpdated := strconv.Itoa((row["UPDATED_TIMESTAMP"]).(int))
+	capacity := strconv.Itoa((row["CAPACITY"]).(int))
+	createdTimestamp := (row["CREATED_TIMESTAMP"]).(time.Time).String()
+
+	s := "NAME => " + name + ", VOLUME => " + volume + ", timeFrame => " + timeFrame + ", lastUpdated => " + lastUpdated + ", capacity => " + capacity + ", createdTimestamp => " + createdTimestamp
+
+	return s, nil
 
 }
