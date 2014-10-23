ad211f03839b7674c4bce3cb622186ee6eec8930
Parag Patel <parag.patel.one@gmail.com>
2014-10-23 09:07:52 -0500

    Added tested functions for the FireTriggerEveryX(Days|Months|Weeks) functions.

--- db/sqlconstants/sqlconstants_test.go
+++ db/sqlconstants/sqlconstants_test.go
@@ -1,8 +1,8 @@
 package sqlconstants
 
 import (
-	"fmt"
 	"testing"
+	"time"
 )
 
 func TestGoType(t *testing.T) {
@@ -16,5 +16,70 @@ func TestGoType(t *testing.T) {
 		return
 	}
 
-	fmt.Println("CONSTANT VALUES => ", SQLITE3_TYPE_NULL, SQLITE3_TYPE_INTEGER)
+}
+
+func TestFireTriggerEveryXDaysFrom(t *testing.T) {
+
+	const normal = "2006-01-02 15:04"
+
+	tn, err := time.Parse(normal, "2011-02-27 04:40")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	s, err := FireTriggerEveryXDaysFrom(3, tn)
+
+	if err != nil {
+		t.Error("Error =>", err)
+	}
+
+	if s != "DATETIME('2011-02-27 04:40','+3 days')" {
+		t.Error("Expected SQL did not match ")
+	}
+
+}
+
+func TestFireTriggerEveryXMonthsFrom(t *testing.T) {
+
+	const normal = "2006-01-02 15:04"
+
+	tn, err := time.Parse(normal, "2011-02-27 04:40")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	s, err := FireTriggerEveryXMonthsFrom(3, tn)
+
+	if err != nil {
+		t.Error("Error =>", err)
+	}
+
+	if s != "DATETIME('2011-02-27 04:40','+3 months')" {
+		t.Error("Expected SQL did not match ")
+	}
+
+}
+
+func TestFireTriggerEveryXWeeksFrom(t *testing.T) {
+
+	const normal = "2006-01-02 15:04"
+
+	tn, err := time.Parse(normal, "2011-02-27 04:40")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	s, err := FireTriggerEveryXWeeksFrom(3, tn)
+
+	if err != nil {
+		t.Error("Error =>", err)
+	}
+
+	if s != "DATETIME('2011-02-27 04:40','+21 days')" {
+		t.Error("Expected SQL did not match ")
+	}
+
 }
